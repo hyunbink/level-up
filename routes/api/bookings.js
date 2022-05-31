0579edae6f5205c2d4f3461f839bff20d3d22a3a
Matia Hyunbin Kim <matia.h.kim@gmail.com>
2022-05-31 15:45:36 -0700

    bookings create backend

--- routes/api/bookings.js
+++ routes/api/bookings.js
@@ -2,3 +2,28 @@ const express = require("express");
 const router = express.Router();
 const Booking = require("../../models/Booking");
 const User = require("../../models/User");
+const validateBookingInput = require("../../validation/booking");
+
+router.post("/create", (req, res) => {
+    const { errors, isValid } = validateBookingInput(req.body);
+
+    if (!isValid) {
+        return res.status(400).json(errors);
+    }
+
+    const newBooking = new Booking({
+        bookingStudId: req.body.bookingStudId,
+        bookingProfId: req.body.bookingProfId,
+        title: req.body.title,
+        date: req.body.date, 
+        duration: req.body.duration 
+    });
+
+    newBooking.save()
+        .then(booking => res.json(booking))
+        .catch(err => console.log(err));
+
+});
+
+
+router.get("")
