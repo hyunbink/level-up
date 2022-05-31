11f0e59996849d5f28a25ee41be5b72ab3bd2ac2
samuelm <smbra33@gmail.com>
2022-05-30 18:06:49 -0700

    create category page

--- frontend/src/components/video/video_index/video_index.jsx
+++ frontend/src/components/video/video_index/video_index.jsx
@@ -5,17 +5,6 @@ import { Link } from "react-router-dom";
 class VideoIndex extends React.Component {
     constructor(props) {
         super(props)
-        this.formatCategoryName = this.formatCategoryName.bind(this);
-    }
-
-    capitalize(word) {
-        return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
-    }
-
-    formatCategoryName() {
-        let category = this.props.category;
-        let words = category.split("-")
-        return words.map(word => (this.capitalize(word))).join(" ");
     }
 
     render() {
@@ -26,26 +15,17 @@ class VideoIndex extends React.Component {
             prevPage = "category"
         }
         return (
-            <div className="video-index-page">
-                <div className="video-index-container">
-                    <div className="video-index-header">
-                        <img src=""/>
-                        <h1>{this.formatCategoryName()}</h1>
-
-                    </div>
-                    <ul className="video-list">
-                        {
-                            this.props.videos.map((video, idx) => (
-                                <VideoIndexItem
-                                    prevPage={prevPage}
-                                    video={video}
-                                    key={`video-${idx}`}
-                                />
-                            ))
-                        }
-                    </ul>
-                </div>
-            </div>
+            <ul className="video-list">
+                {
+                    this.props.videos.map((video, idx) => (
+                        <VideoIndexItem
+                            prevPage={prevPage}
+                            video={video}
+                            key={`video-${idx}`}
+                        />
+                    ))
+                }
+            </ul>
         )
     }
 }
