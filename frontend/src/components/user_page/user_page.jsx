97e4e06d986f0b87238b36e1873fb9919c0b73b2
Matia Hyunbin Kim <matia.h.kim@gmail.com>
2022-06-01 21:10:18 -0700

    update user-page-format

--- frontend/src/components/user_page/user_page.jsx
+++ frontend/src/components/user_page/user_page.jsx
@@ -54,14 +54,10 @@ class UserPage extends React.Component {
             <div className="user-page">
                 <div className="user-container">
                     <div className="user-show-banner">
-
                             {/* <img className="user-show-banner-img" src="../../images/demo_banner.png" alt="user-show-banner-img"></img> */}
-
                     </div>
-
                     <div className="user-show-info-div">
                         <div className="user-show-info-div-left">
-
                                 {/* <div className="user-info"> */}
                                     <div className="user-essential">
                                         <span className="user-photo-">
@@ -76,29 +72,8 @@ class UserPage extends React.Component {
                                         <h1>Interests: <ul className="user-show-interests-list">{this.props.user.interests.split(",").map((int, i)=>(
                                             <li key={`interest0${i}`} className="user-show-interest">{int}</li>
                                         ))}</ul></h1>
-
-                            {/* <div className="user-info"> */}
-                                <div className="user-essential">
-                                    <span className="user-photo-">
-                                        <img className="user-show-profile-photo" alt="user-show-profile"></img>
-                                    </span>
-                                    <span className="user-show-full-name"> {this.props.user.firstName} {this.props.user.lastName} </span>
-                                    <span>{this.props.user.professional ? <BsFillCheckCircleFill className="user-show-prof-icon" />: <div></div> }</span>
-                                    <p></p>
-                                    <br/>
-                                    <p>About: {this.props.user.bio}</p>
-                                    <br/>
-                                    <div>Interests: <ul className="user-show-interests-list">{this.props.user.interests.split(",").map((int,idx)=>(
-                                        <li key={idx} className="user-show-interest">{int}</li>
-                                    ))}</ul>
-
-                                    </div>
-                                </div>
-                                {/* <br /> */}
-                            {/* </div> */}
-                                    
                         </div>
-
+                            </div>
                                 <div className="user-show-info-div-right">
                                     <div className="user-details">
                                         {this.props.user.professional? 
@@ -129,9 +104,8 @@ class UserPage extends React.Component {
                                         </ul>
                                     </div>
                                 </div>
-                                
-                            </div>
-                                      
+                                    
+                </div>
                     <div className="user-reviews-container">
                         <h1>Reviews Container</h1>
                         {this.props.reviews.data ? <ul>{this.props.reviews.data.map((review, idx)=> (
@@ -142,8 +116,6 @@ class UserPage extends React.Component {
                     <div className="create-review-form">
                         <ReviewFormContainer reviewer={this.props.currentUser} reviewee={this.props.user} getReviews={this.getUserReviews}/>
                     </div>
-
-                </div>
                 </div>
             </div>
         )
