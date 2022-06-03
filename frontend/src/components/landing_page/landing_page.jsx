dbdbe6c3b631f93bfaf23f6b7fb08240fe718b4a
echin522 <echin522@gmail.com>
2022-06-03 05:01:19 -0700

    Complete landing page rough draft

--- frontend/src/components/landing_page/landing_page.jsx
+++ frontend/src/components/landing_page/landing_page.jsx
@@ -14,7 +14,8 @@ class LandingPage extends Component {
     };
 
     componentDidMount() {
-        document.querySelector(".body").id = "home"
+        document.querySelector(".body").id = "home";
+        document.querySelector("footer").id = "home";
         setInterval(() => {
             this.setState({
                 textIndex: this.state.textIndex + 1,
@@ -64,7 +65,7 @@ class LandingPage extends Component {
                     </div>
 
                     <div className="card">
-                        <span><img src="https://static.vecteezy.com/system/resources/thumbnails/000/240/170/small/boy-with-glasses.jpg" alt="flaticon.com" loading="lazy"/></span>
+                        <span><img src="https://cdn1.iconfinder.com/data/icons/professional-avatars-5/64/programmer-programming-developer-designer-avatar-512.png" alt="flaticon.com" loading="lazy"/></span>
                         <span>
                             <h1 className="title">Meet real experts</h1>
                             <p>
@@ -84,11 +85,77 @@ class LandingPage extends Component {
                             </p>
                         </span>
                     </div>
+                </div>
 
+                <div className="landing-section-two">
+                    <div className="about">
+                        <h2>About Us</h2>
+                        <h1 className="title">Providing an all-in-one platform for hobbyists to connect with long-time veterans</h1>
+                        <p>If Morbius has a trillion fans I am one of them. If Morbius has 10 fans I am one of them. If Morbius has no fans, that I means I am no longer on Earth. If the Universe is against Morbius, I am against the Universe. I love Morbius until my last breath.</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                    </div>
+                    <div>
+                        <img className="section-banner" src="https://img.freepik.com/free-vector/man-studying-galaxy-through-telescope-women-holding-planets-models-watching-meteors-constellation-stars-flat-vector-illustration-horoscope-astronomy-discovery-astrology-concepts_74855-10106.jpg?w=2000" alt="freepik.com" loading="lazy"/>
+                    </div>
                 </div>
 
 
-                
+                <div className="landing-section-three">
+                    <img className="section-banner" src="https://yggdrasille.files.wordpress.com/2018/08/dark-souls-3-template.jpg?w=768" alt="freepik.com" loading="lazy"/>
+                    <div className="why-us">
+                        <h2>Why Us</h2>
+                        <h1 className="title">Just as your platform content targets humans, it is just practical to leave the job to a human</h1>
+                        <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</h3>
+                        <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </h3>
+                    </div>
+                </div>
+
+                <div className="landing-section-four">
+                    <div className="section-header">
+                        <h1>Our Communities</h1>
+                        <h2>
+                            A little peek into our many worlds
+                        </h2>
+                    </div>
+                    <div className="card-container">
+
+                        <div className="card">
+                            <img src="https://cdn.shopify.com/s/files/1/0050/7091/3651/products/Red-Fishbone-Pinto-Caridina-Shrimp_1200x.png?v=1613190445" alt="flaticon.com" loading="lazy"/>
+                            <h1 class="title">Shrimp breeding and caretaking</h1>
+                        </div>
+
+                        <div className="card">
+                            <img src="https://i.nextmedia.com.au/frankie/shoes3.jpg" alt="flaticon.com" loading="lazy"/>
+                            <h1 class="title">Shoe dyeing</h1>
+                        </div>
+
+                        <div className="card">
+                            <img src="https://cdn.shopify.com/s/files/1/0741/2319/articles/OT_deluxe_black_07_800x.jpg?v=1577995882" alt="flaticon.com" loading="lazy"/>
+                            <h1 class="title">The art of the Otamatone</h1>
+                        </div>
+
+                        <div className="card">
+                            <img src="https://i.pinimg.com/550x/91/5f/be/915fbe23181642b625c194e25b53f214.jpg" alt="flaticon.com" loading="lazy"/>
+                            <h1 class="title">Hardware and software modding</h1>
+                        </div>
+
+                        <div className="card">
+                            <img src="https://paulickreport.com/wp-content/uploads/2021/05/Ostrich-races-LAD.jpg" alt="flaticon.com" loading="lazy"/>
+                            <h1 class="title">Ostrich racing</h1>
+                        </div>
+
+                        <div className="card">
+                            <img src="https://3.bp.blogspot.com/-9D7NM8YGne0/Wr-g7cEHU-I/AAAAAAAADJ8/5l0VzKRncgcZda1WZ_jMoUyNA6eBJwJZgCLcBGAs/s1600/detailed-rg-unicorn-gundam%25252B%252525282%25252529%2B%2528Copy%2529.jpg" alt="flaticon.com" loading="lazy"/>
+                            <h1 class="title">Gundam Plastic Models</h1>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="landing-section-five">
+                    <div className="cofounders-header">
+                        <h1>Project Members</h1>
+                    </div>
+                </div>
             </div>
         )
     }
