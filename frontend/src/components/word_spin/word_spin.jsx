cea5ab5a3c3af83fa2d64c04bff60f050b84b744
echin522 <echin522@gmail.com>
2022-06-07 16:43:47 -0700

    Refactor text transitions

--- frontend/src/components/word_spin/word_spin.jsx
+++ frontend/src/components/word_spin/word_spin.jsx
@@ -1,24 +1,66 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import ReactTextTransition, { presets } from "react-text-transition";
 import "./word_spin.scss";
 
-class WordSpin extends React.Component {
+// class WordSpin extends React.Component {
 
-    render() {
-        return (
-            <div className="word-spin-container">
+//     render() {
+//         return (
+//             <div className="word-spin-container">
+//                 <div className="wrapper">
+//                     Have you ever heard of <p>..................................</p>?
+//                     <div className="spinner-words">
+//                         <span>drone racing</span>
+//                         <span>shoe dyeing</span>
+//                         <span>kendo</span>
+//                         <span>shrimp breeding</span>
+//                         <span>drone racing</span>
+//                     </div>
+//                 </div>
+//             </div>
+//         )
+//     }
+// }
+
+const texts = [
+    "drone racing",
+    "shoe dyeing",
+    "kendo",
+    "shrimp breeding",
+    "ostrich runs",
+    "gundams"
+]
+
+function WordSpin() {
+    const [ textIndex, setCount ] = useState(0);
+
+    useEffect(() => {
+        setInterval(() => {
+            setCount(textIndex + 1)
+        }, 2000);
+    });
+
+    return (      
+        <div className="word-spin-container">
                 <div className="wrapper">
-                    Have you ever heard of <p>..................................</p>?
-                    <div className="spinner-words">
+                    Have you ever heard of 
+                    <ReactTextTransition
+                        className="spinner-text"
+                        text={texts[textIndex % texts.length]}
+                        springConfig={presets.gentle}
+                        style={{ margin: "0 10px" }}
+                        inline
+                    />?
+                    {/* <div className="spinner-words">
                         <span>drone racing</span>
                         <span>shoe dyeing</span>
                         <span>kendo</span>
                         <span>shrimp breeding</span>
                         <span>drone racing</span>
-                    </div>
+                    </div> */}
                 </div>
-            </div>
-        )
-    }
+            </div>  
+    )
 }
 
 export default WordSpin;
