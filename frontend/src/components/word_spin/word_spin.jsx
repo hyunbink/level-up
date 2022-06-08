bd0a33c7e5e79e65b4e4f771bdc7d7de868b7e60
echin522 <echin522@gmail.com>
2022-06-07 17:52:53 -0700

    Fix turbo scroll bug

--- frontend/src/components/word_spin/word_spin.jsx
+++ frontend/src/components/word_spin/word_spin.jsx
@@ -1,66 +1,79 @@
-import React, { useState, useEffect } from "react";
+import React, { Component, useState, useEffect } from "react";
 import ReactTextTransition, { presets } from "react-text-transition";
 import "./word_spin.scss";
 
-// class WordSpin extends React.Component {
 
-//     render() {
-//         return (
-//             <div className="word-spin-container">
-//                 <div className="wrapper">
-//                     Have you ever heard of <p>..................................</p>?
-//                     <div className="spinner-words">
-//                         <span>drone racing</span>
-//                         <span>shoe dyeing</span>
-//                         <span>kendo</span>
-//                         <span>shrimp breeding</span>
-//                         <span>drone racing</span>
-//                     </div>
-//                 </div>
-//             </div>
-//         )
-//     }
-// }
 
 const texts = [
-    "drone racing",
     "shoe dyeing",
     "kendo",
     "shrimp breeding",
-    "ostrich runs",
-    "gundams"
+    "droning",
+    // "ostrich runs",
+    // "gundams",
 ]
 
-function WordSpin() {
-    const [ textIndex, setCount ] = useState(0);
+class WordSpin extends Component {
+    state = {
+        textIndex: 0,
+    }
 
-    useEffect(() => {
+    componentDidMount() {
         setInterval(() => {
-            setCount(textIndex + 1)
-        }, 2000);
-    });
+            this.setState({
+                textIndex: this.state.textIndex + 1,
+            })
+        }, (20000 / texts.length));
+    }
 
-    return (      
-        <div className="word-spin-container">
+    render() {
+        return (
+            <div className="word-spin-container">
                 <div className="wrapper">
                     Have you ever heard of 
                     <ReactTextTransition
                         className="spinner-text"
-                        text={texts[textIndex % texts.length]}
-                        springConfig={presets.gentle}
-                        style={{ margin: "0 10px" }}
+                        text={texts[this.state.textIndex % (texts.length + 1)]}
+                        springConfig={{tension: 300, friction: 100}}
+                        style={{ margin: "0 0px 0 10px" }}
                         inline
                     />?
-                    {/* <div className="spinner-words">
-                        <span>drone racing</span>
-                        <span>shoe dyeing</span>
-                        <span>kendo</span>
-                        <span>shrimp breeding</span>
-                        <span>drone racing</span>
-                    </div> */}
                 </div>
             </div>  
-    )
+        )
+    }
 }
+
+// function WordSpin() {
+//     const [ textIndex, setCount ] = useState(0);
+
+//     useEffect(() => {
+//         setInterval(() => {
+//             setCount(textIndex + 1)
+//         }, 2000);
+//     }, []);
+
+//     return (
+//         <div className="word-spin-container">
+//             <div className="wrapper">
+//                 Have you ever heard of 
+//                 <ReactTextTransition
+//                     className="spinner-text"
+//                     text={texts[textIndex % (texts.length + 1)]}
+//                     springConfig={{tension: 300, friction: 10}}
+//                     style={{ margin: "0 0 0 10px" }}
+//                     inline
+//                 />?
+//                 {/* <div className="spinner-words">
+//                     <span>drone racing</span>
+//                     <span>shoe dyeing</span>
+//                     <span>kendo</span>
+//                     <span>shrimp breeding</span>
+//                     <span>drone racing</span>
+//                 </div> */}
+//             </div>
+//         </div>  
+//     )
+// }
 
 export default WordSpin;
