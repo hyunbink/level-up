cc9f288dbce4c06c47741d9f8c7d6ce91a4dcae7
Matia Hyunbin Kim <matia.h.kim@gmail.com>
2022-06-15 11:45:12 -0700

    video upload remove querySelectors

--- frontend/src/components/video/video_form/video_form.jsx
+++ frontend/src/components/video/video_form/video_form.jsx
@@ -22,12 +22,12 @@ class VideoForm extends React.Component {
         this.handleErrors = this.handleErrors.bind(this);
     }
 
-    componentWillUnmount() {
-        document.querySelector(".sidebar-container").classList.remove("hidden")
-    }
+    // componentWillUnmount() {
+    //     document.querySelector(".sidebar-container").classList.remove("hidden")
+    // }
 
     componentDidMount() {
-        document.querySelector(".sidebar-container").classList.add("hidden")
+        // document.querySelector(".sidebar-container").classList.add("hidden")
         let videoId = this.props.match.params.videoId;
         let video;
         if (videoId) {
