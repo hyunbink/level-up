66ac9e8196af61c0865e1cea7aa7f5813c13e83c
andrew <aaayin@gmail.com>
2022-06-15 09:51:42 -0700

    errors handling

--- frontend/src/components/review/review_form.jsx
+++ frontend/src/components/review/review_form.jsx
@@ -55,7 +55,7 @@ class CreateReviewForm extends React.Component {
 
     handleSubmit(e) {
         e.preventDefault();
-        let reviewFormErrors = document.getElementById("review-form-errors") ;
+        let reviewFormErrors = document.getElementById("review-form-errors");
         reviewFormErrors.classList.remove("hidden");
         document.addEventListener("click", ()=> {
             reviewFormErrors.classList.add("hidden");
