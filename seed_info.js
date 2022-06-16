89e71c2852bbd6bd57bb55302752ecd37c0dc835
echin522 <echin522@gmail.com>
2022-06-16 02:25:30 -0700

    We do a lil seedin

---
+++ seed_info.js
@@ -0,0 +1,161 @@
+
+const videoDescriptions = {
+    "shrimp": [
+        "Neocaridina davidi is a freshwater shrimp originating from eastern China and introduced in Taiwan, Japan, and Hawaii. In the wild, they are typically a greeish-brown color, but in captivity...",
+        "Neos and Sulawesi are two radically different shrimps with wildly different sets of tank parameters. Keeping them together is typically a death sentence for on or both of the shrimps, but today I'm going to show you how I make it work.",
+        "The path to true beauty is one of sacrifice and bloodshed. To get the patterns and colors that you want, you will have to cull your shrimp. This video will show you how to do it as humanely as possible and eventually get your desired shrimps.",
+        "Although a well established tank should be self-sufficient, it's always helpful to supplement your shrimps' diets with various types of nutrient-rich foods. Today's video walks you through some of my favorites, as well as community recommended foods that are proven to produce healthier and more colorful shrimp.",
+        "Unfortunately, many fish stores and plant suppliers have tanks that, at some point or another, become hosts to a variety of parasites and unwanted guests. Those pests will hitch a ride on the plants and animals that eventually get into your tank. It's important to quarantine everything you purchase before setting them into your own aquariums"
+    ],
+    "hedgehogs": [
+        "Hehehe belly rub go brrrrrr",
+        "Somehow, hedgehogs still get mixed up with these much less friendly creatures all the time. It's time that I put an end to this madness >:(",
+        "I can't believe I have to make this video, but I definitely do",
+        "In addition to being illegal in many states, hedgehogs can be a bit more than people can handle as a first pet. It's important to realize what you're getting into beforehand, and to give your potential new friend the best home it can have.",
+        "Don't call the feds on me. I promise you everything I say in this video is legal. At most, they are a light shade of morally grey."
+    ],
+    "raccoons": [
+        "",
+        "",
+        "",
+        "",
+        "",
+    ],
+	"sugar-gliders": [
+    ],
+    "shoe-dyeing": [
+    ],
+    "felting": [
+        "",
+        "",
+        "",
+        "",
+        ""
+    ],
+    "gundam": [
+        "High grade, Master grade, Perfect grade, Real grade... No grade?!?! This hobby is confusing as hell for every newcomer. This brief overview will provide you with everything you need to dip your toes into this hobby",
+        "HHAHAHA IS THAT OPTIMUS PRIME?? YOU STILL PLAY WITH TRANSFORMERS???? I'm sick and tired of hearing this joke. So, I'm going to go into the complete history of both franchises as well as the differences in the big robots (Spoiler: there's a lot)",
+        "This quick little tutorial will bring out a lot of the subtle details that are already in your kits! You can purchase all of the materials you see in this video here: https://www.usagundamstore.com/products/tamiya-america-inc-panel-line-accent-color-light-gray",
+        "The answer basically boils down to how much more work are you willing to do outside of what you get in the box",
+        "If Morbius has a million fans, I am one of them. If Morbius has 5 fans, I am one of them. If Morbius has one fan, That one is me. If Morbius has no fans, I am no longer alive. If the world is against Morbius, I am against the world. Till my last breath, I'll love Morbius (2022)."
+    ],
+    "astrophysics": [
+    ],
+    "ufo-sightings": [
+    ],
+    "telescopes": [
+    ],
+    "otamatone": [
+    ],
+    "zither": [
+    ],
+    "recorder": [
+    ],
+    "kazoo": [
+    ],
+    "sock-puppets": [
+    ],
+    "dungeons-and-dragons": [
+    ],
+    "cosplay": [
+    ],
+    "speedrunning": [
+    ],
+    "modding": [
+    ],
+    "lore": [
+    ],
+    "buzkashi": [
+    ],
+    "speedwalking": [
+    ],
+    "ostrich-racing": [
+    ],
+    "smart-mirrors": [
+    ],
+    "computer-mice": [
+    ],
+    "macro-pad": [
+    ],
+};
+
+
+const videoTitles = {
+    "shrimp": [
+        "Beginner's Guide to Neocaridina",
+        "Neo and Sulawesi Tank Parameters",
+        "How to breed prettier shrimp",
+        "What should I be feeding my skrimps?",
+        "Quarantining your tank"
+    ],
+    "hedgehogs": [
+        "best way to give your hedgie a belly rub",
+        "Differences between a hedgehog and a porcupine",
+        "Please do NOT paint your hedgehog blue",
+        "Are Hedgehogs the Right Pet for You?",
+        "How you can (possibly) raise a hedgehog in California",
+    ],
+    "raccoons": [
+        "Aren't raccoons pests?",
+        "Why you should raise a raccoon!",
+        "Basic raccoon care (it's super easy!)",
+        "Where do I even get a raccoon from?",
+        "Please do not pick up a wild raccoon from your backyard."
+    ],
+	"sugar-gliders": [
+    ],
+    "shoe-dyeing": [
+    ],
+    "felting": [
+        "How to not injure yourself when beginning this hobby",
+        "7 Cool Ideas to Build Up Your Fundamentals!",
+        "Why I LOVE Felting!",
+        "Why felting is a GREAT hobby for people on a budget",
+        "How long does it take to make a T-Rex out of wool?"
+    ],
+    "gundam": [
+        "Where the $#@& do I start???",
+        "No. For the last time, these are NOT Transformers",
+        "How to panel line your first Gunpla",
+        "Which grade is right for you",
+        "What to do if you break a piece of your kit",
+    ],
+    "astrophysics": [
+    ],
+    "ufo-sightings": [
+    ],
+    "telescopes": [
+    ],
+    "otamatone": [
+    ],
+    "zither": [
+    ],
+    "recorder": [
+    ],
+    "kazoo": [
+    ],
+    "sock-puppets": [
+    ],
+    "dungeons-and-dragons": [
+    ],
+    "cosplay": [
+    ],
+    "speedrunning": [
+    ],
+    "modding": [
+    ],
+    "lore": [
+    ],
+    "buzkashi": [
+    ],
+    "speedwalking": [
+    ],
+    "ostrich-racing": [
+    ],
+    "smart-mirrors": [
+    ],
+    "computer-mice": [
+    ],
+    "macro-pad": [
+    ],
+};
